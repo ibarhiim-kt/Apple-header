75c465b58251b01ceadcdf239ee49cc124bca94a
ibarhiim-kt <contact.ibrahiim@gmail.com>
2024-10-19 23:05:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header";
+import WatchSeries from "./Components/WatchSeries";
+function App() {
+  return (
+    <div>
+      <Header/>
+      <WatchSeries/>
+    </div>
+  );
+}
+
+export default App;
